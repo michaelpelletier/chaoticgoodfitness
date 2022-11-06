bd21ed0c19cb7e64fce062ebfeb5cdab7e87a3bf
michaelpelletier <michael@mpelletier.net>
2022-11-05 20:09:06 -0400

    Fix link nesting bug

--- src/components/CGHeader.vue
+++ src/components/CGHeader.vue
@@ -1,17 +1,21 @@
 <template>
   <header class="header">
-    <g-link to="/" class="logo-section">
-      <g-image alt="Example image" src="~/assets/square.png" width="120" />
+    <div class="logo-section">
+      <g-link to="/">
+        <g-image alt="Example image" src="~/assets/square.png" width="120" />
+      </g-link>
       <div class="logo-text">
-        <h1 class="title">Chaotic Good</h1>
-        <h2 class="subtitle">Fitness</h2>
+        <g-link to="/">
+          <h1 class="title">Chaotic Good</h1>
+          <h2 class="subtitle">Fitness</h2>
+        </g-link>
         <nav class="nav--mobile">
           <g-link class="nav__link" to="/">Home</g-link>
           <g-link class="nav__link" to="/about/">About</g-link>
           <g-link class="nav__link" to="/programs/">Programs</g-link>
         </nav>
       </div>
-    </g-link>
+    </div>
 
     <nav class="nav">
       <g-link class="nav__link" to="/">Home</g-link>
