8f6a6dc3f1e40e6c4ef8c2543137f6abfca52215
michaelpelletier <michael@mpelletier.net>
2022-11-27 16:53:35 -0500

    Fix build maybe

--- gridsome.config.js
+++ gridsome.config.js
@@ -1,11 +1,17 @@
 module.exports = {
   siteName: 'Chaotic Good Fitness',
+  siteUrl: 'https://www.chaoticgoodfitness.com',
 
   plugins: [
     {
       use: '@gridsome/plugin-sitemap',
       options: {
-        exclude: ['/privacy', '/legal']
+        exclude: [],
+        '/*': {
+          changefreq: 'monthly',
+          priority: 0.7,
+          lastmod: '2022-11-27',
+        }
       }
     },
     {
