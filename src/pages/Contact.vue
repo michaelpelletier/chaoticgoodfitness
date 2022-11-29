bb7b6fc848df9687392e8ed8ad3d7106a305ddc3
michaelpelletier <michael@mpelletier.net>
2022-11-29 14:28:27 -0500

    Update email address

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -4,7 +4,7 @@
     <p>If you're ready to set up an orientation call with me, please use my <g-link href="https://calendly.com/kjharbison/new-client-orientation">Calendly</g-link> to schedule a chat!</p>
     <p>Otherwise if you have any questions, please feel free to contact me:</p>
     <p><b>Phone number:</b> 631-379-7074</p>
-    <p><b>Email:</b> keira.harbison@gmail.com</p>
+    <p><b>Email:</b> chaoticgoodfitness@gmail.com</p>
     <p><b>Location:</b> Atlanta, GA</p>
   </Layout>
 </template>
