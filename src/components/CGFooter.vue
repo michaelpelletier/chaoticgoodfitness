47802d8e83bc4a3b97e0e1e44b9b5ce491e3f92c
michaelpelletier <michael@mpelletier.net>
2023-10-06 10:02:18 -0400

    Add Privacy Policy

--- src/components/CGFooter.vue
+++ src/components/CGFooter.vue
@@ -1,49 +1,54 @@
 <template>
   <footer>
-    <div class="footer-text">
-      Tell your own story
+    <div class="footer-row">
+      <div class="footer-text">
+        Tell your own story
+      </div>
+
+      <div class="footer-social-logos">
+        <a
+          href="https://www.facebook.com/ChaoticGoodFitness"
+          title="Connect with us on Facebook"
+          target="_blank"
+        >
+          <svg title="Facebook logo" class="social__icon">
+            <use xlink:href="#social-fb"></use>
+          </svg>
+        </a>
+
+        <a
+          href="https://www.yelp.com/biz/chaotic-good-fitness-atlanta"
+          title="Connect with us on Yelp"
+          target="_blank"
+        >
+          <svg title="Yelp logo" class="social__icon">
+            <use xlink:href="#social-yelp"></use>
+          </svg>
+        </a>
+
+        <a
+          href="https://www.reddit.com/user/ChaoticGoodFitness"
+          title="Connect with us on Reddit"
+          target="_blank"
+        >
+          <svg title="Reddit logo" class="social__icon">
+            <use xlink:href="#social-reddit"></use>
+          </svg>
+        </a>
+
+        <a
+          href="https://www.trainerize.me/profile/chaoticgoodfitness"
+          title="Connect with us on Trainerize"
+          target="_blank"
+        >
+          <svg title="Trainerize logo" class="social__icon">
+            <use xlink:href="#social-trainerize"></use>
+          </svg>
+        </a>
+      </div>
     </div>
-
-    <div class="footer-social-logos">
-      <a
-        href="https://www.facebook.com/ChaoticGoodFitness"
-        title="Connect with us on Facebook"
-        target="_blank"
-      >
-        <svg title="Facebook logo" class="social__icon">
-          <use xlink:href="#social-fb"></use>
-        </svg>
-      </a>
-
-      <a
-        href="https://www.yelp.com/biz/chaotic-good-fitness-atlanta"
-        title="Connect with us on Yelp"
-        target="_blank"
-      >
-        <svg title="Yelp logo" class="social__icon">
-          <use xlink:href="#social-yelp"></use>
-        </svg>
-      </a>
-
-      <a
-        href="https://www.reddit.com/user/ChaoticGoodFitness"
-        title="Connect with us on Reddit"
-        target="_blank"
-      >
-        <svg title="Reddit logo" class="social__icon">
-          <use xlink:href="#social-reddit"></use>
-        </svg>
-      </a>
-
-      <a
-        href="https://www.trainerize.me/profile/chaoticgoodfitness"
-        title="Connect with us on Trainerize"
-        target="_blank"
-      >
-        <svg title="Trainerize logo" class="social__icon">
-          <use xlink:href="#social-trainerize"></use>
-        </svg>
-      </a>
+    <div class="footer-row">
+      © 2023 Chaotic Good Fitness. All rights reserved. <g-link to="/Privacy">Privacy Policy</g-link>
     </div>
   </footer>
 </template>
